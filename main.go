51796124fa8831519aaaef27be0444f5f314f276
Maor Swisa <maorswisa@Maors-MacBook-Pro.local>
2022-01-19 09:04:18 +0200

    Add pow function Added a power of function to main resolve #1

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func main() {
 
 }
@@ -18,4 +20,8 @@ func subtract(num1 int, num2 int) int {
 
 func divide(num1 float64, num2 float64) float64 {
 	return num1 / num2
+}
+
+func powerOf(num1 float64, num2 float64) float64 {
+	return math.Pow(num1, num2)
 }
