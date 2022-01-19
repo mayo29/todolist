038b03c13c81c6ed2e0042dc1ae10ab62207f003
Maor Swisa <maorswisa@Maors-MacBook-Pro.local>
2022-01-19 12:08:23 +0200

    sayHello function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
@@ -20,4 +23,8 @@ func divide(num1 float64, num2 float64) float64 {
 
 func powerOf(num1 float64, num2 float64) float64 {
 	return math.Pow(num1, num2)
+}
+
+func sayHello() {
+	fmt.Println("Hello")
 }
